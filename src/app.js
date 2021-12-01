e1df691b55f49b68945932b63b0f0c2c2d1f69db
bruch0 <lucas.bruch0@gmail.com>
2021-12-01 13:02:54 -0300

    feat: add /genres route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 
+import * as genreController from './controllers/genreController.js';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.route('/genres').post(genreController.createGenre);
 
 export default app;
