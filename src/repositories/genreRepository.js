5c848624437e7ba5684156bcd2a784b84d329e99
bruch0 <lucas.bruch0@gmail.com>
2021-12-01 12:56:53 -0300

    fix: add await and return

--- src/repositories/genreRepository.js
+++ src/repositories/genreRepository.js
@@ -1,7 +1,9 @@
 import connection from '../database/database.js';
 
-const createGenre = ({ name }) => {
-  connection.query('INSERT INTO genres (name) values ($1)', [name]);
+const createGenre = async ({ name }) => {
+  await connection.query('INSERT INTO genres (name) values ($1)', [name]);
+
+  return true;
 };
 
 export { createGenre };
