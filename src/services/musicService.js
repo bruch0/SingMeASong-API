d6f8a7a99453ef0e6a7534ceb441aded3eb015e9
bruch0 <lucas.bruch0@gmail.com>
2021-12-01 16:44:58 -0300

    feat: add validations and genre to the music

--- src/services/musicService.js
+++ src/services/musicService.js
@@ -1,11 +1,30 @@
+import * as musicSchema from '../schemas/musicSchema.js';
+import * as genreRepository from '../repositories/genreRepository.js';
 import * as musicRepository from '../repositories/musicRepository.js';
 
-const createMusic = async ({ name, link }) => {
-  const alreadyExists = await musicRepository.musicExists({ name });
+const createMusic = async ({ name, link, genres }) => {
+  const validation = musicSchema.createMusic.validate({ name, link, genres });
 
-  if (alreadyExists) return 0;
+  if (validation.error) return -2;
 
-  await musicRepository.createMusic({ name, link });
+  const alreadyExists = await musicRepository.musicExists({ link });
+
+  if (alreadyExists) return -1;
+
+  const allGenres = await genreRepository.getGenres();
+  const totalGenres = allGenres.length;
+
+  // creates an array from 1 to the last genreId
+  const genreIds = Array.apply(null, Array(totalGenres)).map(function (x, i) {
+    return i + 1;
+  });
+
+  const checkGenres = genres.map((genre) => genreIds.includes(Number(genre)));
+  const invalidGenres = checkGenres.includes(false);
+
+  if (invalidGenres) return 0;
+
+  await musicRepository.createMusic({ name, link, genres });
 
   return 1;
 };
