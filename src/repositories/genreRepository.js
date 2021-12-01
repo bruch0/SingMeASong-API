0c2ee6daa8dc9ceb5a5531a3b772cae5ca060a3b
bruch0 <lucas.bruch0@gmail.com>
2021-12-01 13:47:31 -0300

    feat: add function to check if a genre already exists

--- src/repositories/genreRepository.js
+++ src/repositories/genreRepository.js
@@ -1,9 +1,18 @@
 import connection from '../database/database.js';
 
+const genreExists = async ({ name }) => {
+  const genres = await connection.query(
+    'SELECT * FROM genres WHERE name = $1',
+    [name]
+  );
+
+  return Boolean(genres.rows.length);
+};
+
 const createGenre = async ({ name }) => {
   await connection.query('INSERT INTO genres (name) values ($1)', [name]);
 
   return true;
 };
 
-export { createGenre };
+export { genreExists, createGenre };
