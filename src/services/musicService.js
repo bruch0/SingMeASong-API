eb6d3f39af8fa97b323457bab4023315ebb0db24
bruch0 <lucas.bruch0@gmail.com>
2021-12-02 14:37:20 -0300

    fix: call error function by its new name

--- src/services/musicService.js
+++ src/services/musicService.js
@@ -3,21 +3,23 @@ import * as genreRepository from '../repositories/genreRepository.js';
 import * as musicRepository from '../repositories/musicRepository.js';
 import * as musicGenresRepository from '../repositories/musicGenresRepository.js';
 import {
-  InvalidMusic,
+  InvalidBody,
   ConflictMusic,
   GenreNotFound,
+  NoMusics,
 } from '../errors/musicErrors.js';
 
 const createMusic = async ({ name, link, genres }) => {
   const validation = musicSchema.createMusic.validate({ name, link, genres });
 
-  if (validation.error) throw new InvalidMusic();
+  if (validation.error) throw new InvalidBody();
 
   const alreadyExists = await musicRepository.musicExists({ link });
 
   if (alreadyExists) throw new ConflictMusic();
 
   const allGenres = await genreRepository.getGenres();
+
   const totalGenres = allGenres.length;
 
   // creates an array from 1 to the last genreId
@@ -40,8 +42,12 @@ const getRecommendation = async () => {
   const randomIndex = (length) => Math.floor(Math.random() * length);
 
   const musics = await musicRepository.getMusics();
+  if (musics.length === 0) throw new NoMusics();
+
   let filteredMusics;
   let index = randomIndex(musics.length);
+  let youtubeLink = musics[index].link;
+  let musicId = musics[index].id;
 
   const lookForWellRated = randomChance();
 
@@ -53,14 +59,17 @@ const getRecommendation = async () => {
 
   if (filteredMusics.length) {
     index = randomIndex(filteredMusics.length);
+    youtubeLink = filteredMusics[index].link;
+    musicId = filteredMusics[index].id;
+    delete filteredMusics[index].link;
   }
 
-  const musicId = filteredMusics[index].id;
   const musicGenres = await musicGenresRepository.getMusicGenres({ musicId });
+  delete musics[index].link;
 
-  return filteredMusics
-    ? { ...filteredMusics[index], genres: musicGenres }
-    : { ...musics[index], genres: musicGenres };
+  return filteredMusics[index]
+    ? { ...filteredMusics[index], genres: musicGenres, youtubeLink }
+    : { ...musics[index], genres: musicGenres, youtubeLink };
 };
 
 export { createMusic, getRecommendation };
