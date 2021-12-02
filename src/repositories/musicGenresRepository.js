fcc6cb3ebbc11ae84d2ab1862f018acfe657cd8e
bruch0 <lucas.bruch0@gmail.com>
2021-12-02 10:41:45 -0300

    feat: add functions to retreive music genres

---
+++ src/repositories/musicGenresRepository.js
@@ -0,0 +1,12 @@
+import connection from '../database/database.js';
+
+const getMusicGenres = async ({ musicId }) => {
+  const genres = await connection.query(
+    'SELECT  music_genres.id, genres.* FROM music_genres JOIN genres ON music_genres.genre_id = genres.id WHERE  music_genres.music_id = $1',
+    [musicId]
+  );
+
+  return genres.rows;
+};
+
+export { getMusicGenres };
