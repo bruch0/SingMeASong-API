88af4bbc4e35ff5094dc3b7118e1b8bdb9f5f505
bruch0 <lucas.bruch0@gmail.com>
2021-12-02 23:14:16 -0300

    feat: add restart entity to make tables word as intended

--- tests/utils/clearDatabase.js
+++ tests/utils/clearDatabase.js
@@ -1,11 +1,11 @@
 import connection from '../../src/database/database';
 
 const clearGenres = async () => {
-  await connection.query('DELETE FROM genres');
+  await connection.query('TRUNCATE genres RESTART IDENTITY');
 };
 
 const clearMusics = async () => {
-  await connection.query('TRUNCATE musics CASCADE');
+  await connection.query('TRUNCATE musics CASCADE RESTART IDENTITY');
 };
 
 export { clearGenres, clearMusics };
