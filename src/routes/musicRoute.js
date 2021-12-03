da84266c228029ce772c707eddee4f8a715b5da9
bruch0 <lucas.bruch0@gmail.com>
2021-12-02 23:31:28 -0300

    feat: add voting to the recommendation route

--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -5,10 +5,12 @@ import * as musicController from '../controllers/musicController.js';
 const router = new Router();
 
 router.group('/recommendations', (route) => {
-  route.post('', musicController.createMusic);
   route.get('/random', musicController.getRecommendation);
   route.get('/top/:limit', musicController.getTopMusics);
   route.get('/top/random');
+  route.post('', musicController.createMusic);
+  route.post('/:musicId/upvote', musicController.voteMusic);
+  route.post('/:musicId/downvote', musicController.voteMusic);
 });
 
 export default router;
