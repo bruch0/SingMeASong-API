5d4320c59372e21a3753dad894c0f45962d2ea2a
bruch0 <lucas.bruch0@gmail.com>
2021-12-04 09:42:11 -0300

    tests: add getMusicsByGenreId unit tests

--- tests/unit/genreService.test.js
+++ tests/unit/genreService.test.js
@@ -2,7 +2,10 @@ import faker from 'faker';
 
 import * as genreService from '../../src/services/genreService.js';
 import * as genreRepository from '../../src/repositories/genreRepository.js';
-import { NoGenres } from '../../src/errors/genreErrors.js';
+import * as musicGenreRepository from '../../src/repositories/musicGenresRepository.js';
+import * as musicRepository from '../../src/repositories/musicRepository.js';
+import { GenreNotFound, NoGenres } from '../../src/errors/genreErrors.js';
+import { NoMusics } from '../../src/errors/musicErrors.js';
 
 describe('genre Service create genre', () => {
   jest.spyOn(genreRepository, 'createGenre').mockImplementation(() => true);
@@ -107,5 +110,69 @@ describe('genre Service get genres', () => {
     const genres = await genreService.getGenres();
 
     expect(genres).toEqual([{ id: 1, name }]);
+  });
+});
+
+describe('genre Service get genres', () => {
+  it('should throw an error when no musics are found', async () => {
+    jest
+      .spyOn(musicGenreRepository, 'getAllMusicGenres')
+      .mockImplementationOnce(() => []);
+
+    const promise = genreService.getAllMusicsByGenre({ genreId: 1 });
+
+    await expect(promise).rejects.toThrow(NoMusics);
+  });
+
+  it('should throw an error when no music has the matching genre', async () => {
+    const genreId = faker.datatype.number();
+    jest
+      .spyOn(musicGenreRepository, 'getAllMusicGenres')
+      .mockImplementationOnce(() => [
+        { id: 1, music_id: 1, genre_id: genreId, name: faker.music.genre() },
+        { id: 2, music_id: 1, genre_id: genreId, name: faker.music.genre() },
+      ]);
+
+    const promise = genreService.getAllMusicsByGenre({
+      genreId: faker.datatype.number(),
+    });
+
+    await expect(promise).rejects.toThrow(GenreNotFound);
+  });
+
+  it('should throw an error when no music has the matching genre', async () => {
+    const genreId = faker.datatype.number();
+    jest
+      .spyOn(musicGenreRepository, 'getAllMusicGenres')
+      .mockImplementationOnce(() => [
+        { id: 1, music_id: 1, genre_id: genreId, name: faker.music.genre() },
+        { id: 2, music_id: 1, genre_id: genreId, name: faker.music.genre() },
+      ]);
+
+    jest.spyOn(musicRepository, 'getMusics').mockImplementationOnce(() => [
+      {
+        id: 1,
+        name: faker.datatype.string(),
+        link: faker.datatype.string(),
+        score: faker.datatype.number(),
+      },
+    ]);
+
+    jest
+      .spyOn(genreRepository, 'getGenreNameById')
+      .mockImplementationOnce(() => faker.music.genre());
+
+    const result = await genreService.getAllMusicsByGenre({
+      genreId,
+    });
+
+    expect(result).toEqual(
+      expect.objectContaining({
+        name: expect.any(String),
+        id: expect.any(Number),
+        score: expect.any(Number),
+        recommendations: expect.anything(),
+      })
+    );
   });
 });
