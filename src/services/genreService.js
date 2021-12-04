3f5c20829928095e82a51e744e7231f6ac9c04b5
bruch0 <lucas.bruch0@gmail.com>
2021-12-04 09:03:07 -0300

    feat: add get all musics by genre id

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,10 +1,14 @@
 import * as genreRepository from '../repositories/genreRepository.js';
+import * as musicRepository from '../repositories/musicRepository.js';
+import * as musicGenresRepository from '../repositories/musicGenresRepository.js';
 import * as genreSchema from '../validations/genreSchema.js';
 import {
   InvalidGenre,
   ConflictGenre,
   NoGenres,
+  GenreNotFound,
 } from '../errors/genreErrors.js';
+import { NoMusics } from '../errors/musicErrors.js';
 
 const createGenre = async ({ name }) => {
   const validation = genreSchema.createGenre.validate({ name });
@@ -34,4 +38,59 @@ const getGenres = async () => {
   return genres;
 };
 
-export { createGenre, getGenres };
+const getAllMusicsByGenre = async ({ genreId }) => {
+  const allMusicGenres = await musicGenresRepository.getAllMusicGenres();
+
+  if (!allMusicGenres.length) throw new NoMusics();
+
+  const musicsIds = allMusicGenres.filter(
+    (musicGenre) => musicGenre.genre_id === Number(genreId)
+  );
+
+  if (!musicsIds.length) throw new GenreNotFound();
+
+  const allMusics = await musicRepository.getMusics();
+
+  const genreMusics = allMusicGenres.filter(
+    (music) => music.genre_id === Number(genreId)
+  );
+
+  const musicIds = genreMusics.map((musicGenre) => musicGenre.music_id);
+
+  const genreName = await genreRepository.getGenreNameById({ genreId });
+  let totalScore = 0;
+  const recommendations = [];
+
+  allMusics.forEach((music) => {
+    if (musicIds.includes(music.id)) {
+      recommendations.push(music);
+    }
+  });
+
+  recommendations.forEach((recommendation) => {
+    const musicGenres = allMusicGenres.filter(
+      (musicGenre) => musicGenre.music_id === recommendation.id
+    );
+
+    musicGenres.forEach((genre) => {
+      genre.id = genre.genre_id;
+      delete genre.music_id;
+      delete genre.genre_id;
+    });
+
+    recommendation.genres = musicGenres;
+    recommendation.youtubeLink = recommendation.link;
+    delete recommendation.link;
+
+    totalScore += recommendation.score;
+  });
+
+  return {
+    id: Number(genreId),
+    name: genreName,
+    score: totalScore,
+    recommendations,
+  };
+};
+
+export { createGenre, getGenres, getAllMusicsByGenre };
